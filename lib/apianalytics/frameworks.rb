5b9c541e0befca7a9d362a28476e0b6d89065e9f
Kenneth Lee <kennethklee@gmail.com>
2015-03-26 10:02:44 -0400

    forgot to remove the text printing

--- lib/apianalytics/frameworks.rb
+++ lib/apianalytics/frameworks.rb
@@ -2,20 +2,17 @@ puts 'hooks'
 
 # == Rack Hook =============================================================
 if (defined?(Rack))
-  puts 'rack!'
   require 'apianalytics/frameworks/rack'
 end
 
 # == Sinatra Hook ==========================================================
 if (defined?(Sinatra))
-  puts 'sinatra!'
   require 'apianalytics/frameworks/sinatra'
 end
 
 # == Rails Hook ============================================================
 
 if (defined?(Rails))
-  puts 'rails!'
   require 'apianalytics/frameworks/rails'
 end
 
